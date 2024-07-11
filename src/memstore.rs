c7e55b61441e60f0846de306b6ea9f3f9f169a32
Matilda Horger <matilda.horger@gmail.com>
2024-07-11 22:27:03 +0000

    Implement storage unifier

--- src/memstore.rs
+++ src/memstore.rs
@@ -6,14 +6,11 @@ use std::collections::HashMap;
 type MemResources = HashMap<Digest, Vec<u8>>;
 impl ResourceStore for MemResources {
     type Err = ();
-    fn load(&mut self, d: &Digest) -> Result<Resource, LoadError<Self::Err>> {
-        match self.get(d) {
-            Some(s) => Ok(Resource::from(s.clone())),
-            None => Err(LoadError::NotFound),
-        }
+    fn load(&mut self, d: &Digest) -> Result<Option<Resource>, Self::Err> {
+        Ok(self.get(d).map(|ptr| ptr.into()))
     }
-    fn save(&mut self, res: Resource) -> Result<(), Self::Err> {
-        self.insert(res.digest, res.body);
+    fn save(&mut self, res: &Resource) -> Result<(), Self::Err> {
+        self.insert(res.digest.clone(), res.body.clone());
         Ok(()) // Cannot fail
     }
 }
@@ -21,12 +18,9 @@ impl ResourceStore for MemResources {
 type MemLabels = HashMap<Vec<u8>, Digest>;
 impl LabelStore for MemLabels {
     type Err = ();
-    fn load(&mut self, label: impl AsRef<[u8]>) -> Result<Digest, LoadError<Self::Err>> {
+    fn load(&mut self, label: impl AsRef<[u8]>) -> Result<Option<Digest>, Self::Err> {
         let k: Vec<u8> = label.as_ref().into();
-        match self.get(&k) {
-            Some(d) => Ok(d.clone()),
-            None => Err(LoadError::NotFound),
-        }
+        Ok(self.get(&k).map(|ptr| ptr.clone()))
     }
     fn save(&mut self, label: impl AsRef<[u8]>, d: &Digest) -> Result<(), Self::Err> {
         self.insert(label.as_ref().into(), d.clone());
@@ -41,13 +35,14 @@ mod test {
     #[test]
     fn resource_round_trip() {
         let mut mr = MemResources::new();
-        let d: Digest = "foo".into();
-        assert_eq!(mr.load(&d), Err(LoadError::NotFound));
+        let rsc: Resource = "foo".to_string().into();
+        let d = rsc.digest.clone();
+        assert_eq!(mr.load(&d).unwrap(), None);
         assert_eq!(mr.exists(&d).unwrap(), false);
 
-        mr.save("foo".to_string().into()).expect("Saving to memory should never fail");
+        mr.save(&rsc).expect("Saving to memory should never fail");
         assert_eq!(mr.exists(&d).unwrap(), true);
-        assert_eq!(mr.load(&d), Ok(Resource::from("foo".to_string())));
+        assert_eq!(mr.load(&d).unwrap(), Some(Resource::from("foo".to_string())));
     }
 
     #[test]
@@ -55,11 +50,20 @@ mod test {
         let mut ml = MemLabels::new();
         let label = "Some label";
         let d: Digest = "foo".into();
-        assert_eq!(ml.load(label), Err(LoadError::NotFound));
+        assert_eq!(ml.load(label).unwrap(), None);
         assert_eq!(ml.exists(label).unwrap(), false);
 
         ml.save(label, &d).expect("Saving to memory should never fail");
         assert_eq!(ml.exists(label).unwrap(), true);
-        assert_eq!(ml.load(label), Ok(d));
+        assert_eq!(ml.load(label).unwrap(), Some(d));
+    }
+
+    #[test]
+    fn storage() {
+        let mut s = Storage::new(MemResources::new(), MemLabels::new());
+        assert_eq!(s.load("some label"), Ok(None));
+
+        s.save("some label", "some bytes").expect("In-memory save");
+        assert_eq!(s.load("some label").unwrap(), Some(Vec::<u8>::from("some bytes")));
     }
 }
--- src/traits.rs
+++ src/traits.rs
@@ -1,43 +1,70 @@
 use crate::digest::Digest;
 use crate::resource::Resource;
 
-#[derive(Debug,PartialEq)]
-pub enum LoadError<T> {
-    NotFound,
-    EngineError(T),
-}
-
 // A content-addressed store which can load or save Resources.
 pub trait ResourceStore {
     type Err;
 
-    fn load(&mut self, d: &Digest) -> Result<Resource, LoadError<Self::Err>>;
-    fn save(&mut self, res: Resource) -> Result<(), Self::Err>;
+    fn load(&mut self, d: &Digest) -> Result<Option<Resource>, Self::Err>;
+    fn save(&mut self, res: &Resource) -> Result<(), Self::Err>;
 
     // In some implementations, is faster than `load`.
     // Generic implementation relies on `load` however.
     fn exists(&mut self, d: &Digest) -> Result<bool, Self::Err> {
-        match self.load(d) {
-            Ok(_) => Ok(true),
-            Err(LoadError::NotFound) => Ok(false),
-            Err(LoadError::EngineError(e)) => Err(e),
-        }
+        Ok(self.load(d)?.is_some())
     }
 }
 
 pub trait LabelStore {
     type Err;
 
-    fn load(&mut self, label: impl AsRef<[u8]>) -> Result<Digest, LoadError<Self::Err>>;
+    fn load(&mut self, label: impl AsRef<[u8]>) -> Result<Option<Digest>, Self::Err>;
     fn save(&mut self, label: impl AsRef<[u8]>, d: &Digest) -> Result<(), Self::Err>;
 
     // In some implementations, is faster than `load`.
     // Generic implementation relies on `load` however.
     fn exists(&mut self, label: impl AsRef<[u8]>) -> Result<bool, Self::Err> {
-        match self.load(label) {
-            Ok(_) => Ok(true),
-            Err(LoadError::NotFound) => Ok(false),
-            Err(LoadError::EngineError(e)) => Err(e),
+        Ok(self.load(label)?.is_some())
+    }
+}
+
+#[derive(PartialEq,Debug)]
+pub enum StorageError<R,L> where R: ResourceStore, L: LabelStore {
+    ResourceError(R::Err),
+    LabelError(L::Err),
+}
+
+type StorageResult<T,R,L> = Result<T, StorageError<R,L>>;
+
+pub struct Storage<R,L> where R: ResourceStore, L: LabelStore {
+    resources: R,
+    labels: L,
+}
+
+impl<R,L> Storage<R,L> where R: ResourceStore, L: LabelStore {
+    pub fn new(resources: R, labels: L) -> Self {
+        Storage { resources: resources, labels: labels }
+    }
+
+    pub fn load(&mut self, label: impl AsRef<[u8]>) -> StorageResult<Option<Vec<u8>>, R,L> {
+        let d = match self.labels.load(label) {
+            Ok(Some(d)) => d,
+            Ok(None) => return Ok(None),
+            Err(e) => return Err(StorageError::LabelError(e)),
+        };
+        match self.resources.load(&d) {
+            Ok(x) => Ok(x.map(|rsc| rsc.body)),
+            Err(e) => Err(StorageError::ResourceError(e))
         }
+    }
+    pub fn save(&mut self, label: impl AsRef<[u8]>, body: impl Into<Resource>) -> StorageResult<(), R,L> {
+        let rsc: Resource = body.into();
+        if let Err(e) = self.resources.save(&rsc) {
+            return Err(StorageError::ResourceError(e))
+        }
+        if let Err(e) = self.labels.save(label, &rsc.digest) {
+            return Err(StorageError::LabelError(e))
+        }
+        Ok(())
     }
 }
