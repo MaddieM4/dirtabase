da10f1b85ae6f244993782625c0f0a8d516a2a38
Matilda Horger <matilda.horger@gmail.com>
2024-07-16 21:10:11 +0000

    Use engine.set_label in main program

--- src/main.rs
+++ src/main.rs
@@ -8,40 +8,36 @@ fn main() {
         .expect("Should store resource");
     println!("Stored object: {:?}", hw.digest);
 
-    let archive = vec![ArchiveEntry {
-        path: "some/file.txt".into(),
-        spec: Spec {
-            format: Format::File,
-            compression: Compression::Plain,
-            digest: hw.digest.clone(),
-        },
-        attrs: vec![
-            Attr::new("unix_owner", "1000"),
-            Attr::new("unix_group", "1000"),
-            Attr::new("unix_flags", "0x777"),
-            Attr::new("frob_value", "absolutely frobnicated"),
-        ],
-    }];
-    let arch_bytes = serde_json::to_vec(&archive).unwrap();
-    let arch_rsc = engine.store(arch_bytes).expect("Store arch_bytes");
-    println!("Stored archive: {:?}", arch_rsc.digest);
-
-    let root = vec![ArchiveEntry {
-        path: "@example".into(),
-        spec: Spec {
-            format: Format::JSON,
-            compression: Compression::Plain,
-            digest: arch_rsc.digest.clone(),
-        },
-        attrs: vec![],
-    }];
-    let root_bytes = serde_json::to_vec(&root).unwrap();
-    let root_rsc = engine.store(root_bytes).expect("Store root_bytes");
-    println!("Stored root archive: {:?}", root_rsc.digest);
-
-    engine.replace_root(None, Some(Spec {
+    let archive = Archive {
         format: Format::JSON,
         compression: Compression::Plain,
-        digest: root_rsc.digest.clone(),
-    })).expect("Failed to store rootdata");
+        entries: vec![ArchiveEntry {
+            path: "some/file.txt".into(),
+            spec: Spec {
+                format: Format::File,
+                compression: Compression::Plain,
+                digest: hw.digest.clone(),
+            },
+            attrs: vec![
+                Attr::new("unix_owner", "1000"),
+                Attr::new("unix_group", "1000"),
+                Attr::new("unix_flags", "0x777"),
+                Attr::new("frob_value", "absolutely frobnicated"),
+            ],
+        }],
+    };
+    let arch_rsc = engine.store(archive.to_buffer()).expect("Store @example archive");
+    println!("Stored @example archive: {:?}", arch_rsc.digest);
+
+    engine.set_label("example", &Spec {
+        format: archive.format,
+        compression: archive.compression,
+        digest: arch_rsc.digest.clone(),
+    }).expect("Failed to store rootdata");
+
+    engine.set_label("example2", &Spec {
+        format: archive.format,
+        compression: archive.compression,
+        digest: arch_rsc.digest,
+    }).expect("Failed to store rootdata");
 }
