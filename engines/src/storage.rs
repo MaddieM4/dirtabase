f81c1444d281703232b7a1606d9b5e2a3eaa3f70
Matilda Horger <matilda.horger@gmail.com>
2024-07-16 20:58:18 +0000

    Significant higher-level API work

--- engines/src/storage.rs
+++ engines/src/storage.rs
@@ -24,4 +24,95 @@ pub trait Store {
         self.save(&rsc.digest, &rsc.body)?;
         Ok(rsc)
     }
+
+    fn load_archive(&mut self, spec: &Option<Spec>) -> Result<Archive, Self::Error> {
+        let archive: Option<Archive> = match &spec {
+            None => None,
+            Some(s) => match self.load(&s.digest)? {
+                None => None,
+                Some(buf) => Some(Archive::from_buffer(s.format, s.compression, &buf)),
+            }
+        };
+
+        Ok(archive.unwrap_or(Archive {
+            format: Format::JSON,
+            compression: Compression::Plain,
+            entries: vec![],
+        }))
+    }
+
+    fn set_label(&mut self, name: &str, spec: &Spec) -> Result<(), Self::Error> {
+        let entry = ArchiveEntry {
+            path: format!("@{}", name).into(),
+            spec: spec.clone(),
+            attrs: vec![],
+        };
+        for _attempt in 0..10 {
+            let original_root_spec = self.read_root()?;
+            let mut archive = self.load_archive(&original_root_spec)?;
+            archive.set(&entry);
+
+            let rsc = self.store(&archive)?;
+            let rd = Some(Spec {
+                format: Format::JSON,
+                compression: Compression::Plain,
+                digest: rsc.digest.clone(),
+            });
+
+            if self.replace_root(original_root_spec, rd)? {
+                return Ok(())
+            }
+        }
+        panic!("After multiple attempts, could not set label");
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::memory::Memory;
+
+    #[test]
+    fn load_archive() {
+        let mut store = Memory::new();
+        let archive = Archive {
+            format: Format::JSON,
+            compression: Compression::Plain,
+            entries: vec![
+                ArchiveEntry {
+                    path: "foo".into(),
+                    spec: Spec {
+                        format: Format::File,
+                        compression: Compression::Plain,
+                        digest: "foo".into(),
+                    },
+                    attrs: vec![],
+                },
+            ],
+        };
+        let rsc: Resource = (&archive).into();
+        let spec = Spec {
+            format: archive.format,
+            compression: archive.compression,
+            digest: rsc.digest,
+        };
+
+        // No spec provided: default archive
+        assert_eq!(store.load_archive(&None).unwrap(), Archive {
+            format: Format::JSON,
+            compression: Compression::Plain,
+            entries: vec![],
+        });
+
+        // Spec not found: default archive
+        assert_eq!(store.load_archive(&Some(spec.clone())).unwrap(), Archive {
+            format: Format::JSON,
+            compression: Compression::Plain,
+            entries: vec![],
+        });
+
+        // Spec found: restore archive
+        let _ = store.store(&archive).unwrap();
+        assert_eq!(store.load_archive(&Some(spec)).unwrap(), archive);
+    }
 }
--- types/src/primitives.rs
+++ types/src/primitives.rs
@@ -17,14 +17,14 @@ impl<T> From<T> for Resource where T: AsRef<[u8]> {
     }
 }
 
-#[derive(PartialEq,Debug,Clone,Serialize,Deserialize)]
+#[derive(PartialEq,Debug,Clone,Copy,Serialize,Deserialize)]
 #[serde(rename_all="lowercase")]
 pub enum Format {
     File,
     JSON,
 }
 
-#[derive(PartialEq,Debug,Clone,Serialize,Deserialize)]
+#[derive(PartialEq,Debug,Clone,Copy,Serialize,Deserialize)]
 #[serde(rename_all="lowercase")]
 pub enum Compression {
     Plain,
@@ -38,7 +38,7 @@ pub struct Spec {
 }
 pub type RootData = Option<Spec>;
 
-#[derive(PartialEq,Debug,Serialize,Deserialize)]
+#[derive(PartialEq,Debug,Clone,Serialize,Deserialize)]
 pub struct Attr(String,String);
 impl Attr {
     pub fn new(name: impl AsRef<str>, value: impl AsRef<str>) -> Self {
@@ -46,11 +46,48 @@ impl Attr {
     }
 }
 
-#[derive(PartialEq,Debug,Serialize,Deserialize)]
+#[derive(PartialEq,Debug,Clone,Serialize,Deserialize)]
 pub struct ArchiveEntry {
     pub path: String,
     pub spec: Spec,
     pub attrs: Vec<Attr>,
+}
+
+#[derive(PartialEq,Debug,Clone,Serialize,Deserialize)]
+pub struct Archive {
+    pub format: Format,
+    pub compression: Compression,
+    pub entries: Vec<ArchiveEntry>,
+}
+
+impl From<&Archive> for Resource {
+    fn from(archive: &Archive) -> Self {
+        archive.to_buffer().into()
+    }
+}
+
+impl Archive {
+    pub fn set(&mut self, entry: &ArchiveEntry) {
+        self.entries.retain(|e| e.path != entry.path);
+        self.entries.push(entry.clone());
+    }
+
+    pub fn to_buffer(&self) -> Buffer {
+        assert!(self.format == Format::JSON);
+        assert!(self.compression == Compression::Plain);
+        serde_json::to_vec(&self.entries).unwrap()
+    }
+
+    pub fn from_buffer(format: Format, compression: Compression, buf: &Buffer) -> Self {
+        assert!(format == Format::JSON);
+        assert!(compression == Compression::Plain);
+        let entries: Vec<ArchiveEntry> = serde_json::from_slice(buf).unwrap();
+        Archive {
+            format: format,
+            compression: compression,
+            entries: entries,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -76,29 +113,75 @@ mod test {
 
     #[test]
     fn serde_archive() -> serde_json::Result<()> {
-        let archive: Vec<ArchiveEntry> = vec![
-            ArchiveEntry {
-                path: "foo/bar.txt".into(),
-                spec: Spec {
-                  format: Format::File,
-                  compression: Compression::Plain,
-                  digest: "some text".into(),
-                },
-                attrs: vec![
-                  Attr::new("unix_owner", "1000"),
-                  Attr::new("unix_group", "1000"),
-                  Attr::new("unix_flags", "0x777"),
-                  Attr::new("frob_value", "absolutely frobnicated"),
-                ]
-            }
-        ];
+        let archive = Archive {
+            format: Format::JSON,
+            compression: Compression::Plain,
+            entries: vec![
+                ArchiveEntry {
+                    path: "foo/bar.txt".into(),
+                    spec: Spec {
+                      format: Format::File,
+                      compression: Compression::Plain,
+                      digest: "some text".into(),
+                    },
+                    attrs: vec![
+                      Attr::new("unix_owner", "1000"),
+                      Attr::new("unix_group", "1000"),
+                      Attr::new("unix_flags", "0x777"),
+                      Attr::new("frob_value", "absolutely frobnicated"),
+                    ]
+                }
+            ],
+        };
 
-        let text: String = serde_json::to_string(&archive)?;
-        assert_eq!(&text, r#"[{"path":"foo/bar.txt","spec":{"format":"file","compression":"plain","digest":[185,79,111,18,92,121,227,165,255,170,130,111,88,76,16,213,42,218,102,158,103,98,5,27,130,107,85,119,109,5,174,210]},"attrs":[["unix_owner","1000"],["unix_group","1000"],["unix_flags","0x777"],["frob_value","absolutely frobnicated"]]}]"#);
+        let buf = archive.to_buffer();
+        assert_eq!(
+            String::from_utf8(buf.clone()).unwrap(),
+            r#"[{"path":"foo/bar.txt","spec":{"format":"file","compression":"plain","digest":[185,79,111,18,92,121,227,165,255,170,130,111,88,76,16,213,42,218,102,158,103,98,5,27,130,107,85,119,109,5,174,210]},"attrs":[["unix_owner","1000"],["unix_group","1000"],["unix_flags","0x777"],["frob_value","absolutely frobnicated"]]}]"#);
 
-        let deserialized: Vec<ArchiveEntry> = serde_json::from_str(&text)?;
+        let deserialized = Archive::from_buffer(archive.format, archive.compression, &buf);
         assert_eq!(&deserialized, &archive);
 
         Ok(())
+    }
+
+    #[test]
+    fn archive_set() {
+        let mut archive = Archive {
+            format: Format::JSON,
+            compression: Compression::Plain,
+            entries: vec![],
+        };
+
+        let spec = Spec {
+            format: Format::File,
+            compression: Compression::Plain,
+            digest: "some contents".into(),
+        };
+        let entry1 = ArchiveEntry {
+            path: "same/path".into(),
+            spec: spec.clone(),
+            attrs: vec![Attr::new("entry","1")],
+        };
+        let entry2 = ArchiveEntry {
+            path: "different/path".into(),
+            spec: spec.clone(),
+            attrs: vec![Attr::new("entry","2")],
+        };
+        let entry3 = ArchiveEntry {
+            path: "same/path".into(),
+            spec: spec.clone(),
+            attrs: vec![Attr::new("entry","3")],
+        };
+
+        archive.set(&entry1);
+        assert_eq!(archive.entries, vec![entry1.clone()]);
+
+        archive.set(&entry2);
+        assert_eq!(archive.entries, vec![entry1.clone(), entry2.clone()]);
+
+        // Override existing path
+        archive.set(&entry3);
+        assert_eq!(archive.entries, vec![entry2.clone(), entry3.clone()]);
     }
 }
