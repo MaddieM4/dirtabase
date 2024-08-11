62c581559094c724244401f46bcb5ba477818822
Matilda Horger <matilda.horger@gmail.com>
2024-08-10 19:38:16 -0700

    Various devenv enhancements, especially for MacOS and pre-commit hooks

--- devenv.nix
+++ devenv.nix
@@ -1,8 +1,19 @@
-{ pkgs, lib, config, inputs, ... }:
+{ pkgs, lib, ... }:
 
 {
   # https://devenv.sh/packages/
-  packages = [ pkgs.git pkgs.jq pkgs.openssl ];
+  packages =
+    with pkgs;
+    [
+      git
+      jq
+      openssl
+      tree
+    ]
+    ++ lib.optionals pkgs.stdenv.isDarwin [
+      libiconv
+      darwin.apple_sdk.frameworks.SystemConfiguration
+    ];
 
   # https://devenv.sh/languages/
   languages.rust.enable = true;
@@ -18,6 +29,17 @@
   '';
 
   # https://devenv.sh/pre-commit-hooks/
+  pre-commit.hooks.rustfmt.enable = true;
+  pre-commit.hooks.deadnix.enable = true; # dead nix code remover
+  pre-commit.hooks.nixfmt.enable = true;
+  pre-commit.hooks.nixfmt.package = pkgs.nixfmt-rfc-style;
+  pre-commit.hooks.check-merge-conflicts.enable = true;
+  pre-commit.hooks.check-added-large-files.enable = true; # prohibit "very large" files
+  pre-commit.hooks.check-case-conflicts.enable = true; # macos fs is case-insensitive; helps linux
+  pre-commit.hooks.check-json.enable = true;
+  pre-commit.hooks.check-toml.enable = true;
+  pre-commit.hooks.check-yaml.enable = true;
+  pre-commit.hooks.trim-trailing-whitespace.enable = true;
   pre-commit.hooks.shellcheck.enable = true;
 
   # See full reference at https://devenv.sh/reference/options/
