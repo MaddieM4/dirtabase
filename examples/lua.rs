4d0aa316f9420dd62601bb429430405c1e4d76f7
Matilda Horger <matilda.horger@gmail.com>
2024-08-21 22:29:46 -0700

    Make lua example capable of doing an adequate single-app install

--- examples/lua.rs
+++ examples/lua.rs
@@ -1,5 +1,27 @@
 use dirtabase::prelude::*;
 
+/*
+
+I'm using this as a testing ground for full userspace packaging before
+inevitably moving some of the logic to Layover as a separate project.
+
+You can try it by adding this to your ~/.profile:
+
+```
+# Layover support
+export PATH="$HOME/.layover/bin:$PATH"
+export XDG_DATA_DIRS="$XDG_DATA_DIRS:$HOME/.layover"
+```
+
+You may need to log out and log in to see the effects.
+
+Now just run `cargo run --example lua $HOME/.layover` and you should have
+Lua 5.4.7 available on your system, with an XDG desktop entry. Neat, huh?
+
+You can run this without any system installation with just
+`cargo run --example lua` and no further arguments. It builds to ./out.
+*/
+
 fn main() -> Result<()> {
     let db = DB::new("./.dirtabase_db")?;
     let mut log = Logger::new_real();
@@ -21,8 +43,10 @@ fn main() -> Result<()> {
         .cmd_impure("tar zxf *.tar.gz && rm *.tar.gz")?
         .rename("^lua-5.4.7/", "")?
         .cmd_impure("make all test")?
-        .filter("src/lua$")?
-        .rename("src", "bin")?
+        .rename("src/lua$", "bin/lua")?
+        .cmd_impure("convert doc/logo.gif doc/logo.png")?
+        .rename("doc/logo.png$", "icons/lua.png")?
+        .filter("^(bin|icons)")?
         // Stack on the desktop file
         .import(".", ["examples"])?
         .cmd_impure(
